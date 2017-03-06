03c6f5347f702d7c856b23a83fd54665a7e275fa
Diana Arrieta <dianaArrieta@github.com>
2017-03-06 06:24:09 -0600

    modulo descarga ssh

---
+++ mod-integracion_descarga.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+#Se importan los modulos para ssh y scp
+import paramiko
+from scp import SCPClient
+
+#Se crea una instancia de cliente SSH
+ssh_client = paramiko.SSHClient()
+#Establece politica por defecto para buscar la llave de host en el equipo
+ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+#Inicia la conexion a la maquina donde se guardan los archivos csv
+ssh_client.connect('10.0.0.10',22,'diana','hola123.,')
+#Se pasa la conexion ssh a cliente scp
+scp = SCPClient(ssh_client.get_transport())
+#Se descarga el archivo(Modificar a csv)
+scp.get('archivo.txt')
+#Se cierran las conexiones scp y ssh
+scp.close()
+ssh_client.close()
